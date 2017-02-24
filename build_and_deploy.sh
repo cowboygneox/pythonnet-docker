76ba49c02d8a60e5e89aa859b0c34bada4dc5904
Sean Freitag <sfreitag@conserviscorp.com>
2017-02-24 11:47:28 -0600

    Updating to Mono 4.8 and pythonnet 2.2.2.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t cowboygneox/pythonnet:python2.7-mono4.6.2-pythonnet2.1.0 -f Dockerfile-python2.7-mono4.6.2-pythonnet2.1.0 .
-docker build -t cowboygneox/pythonnet:python3.5-mono4.6.2-pythonnet2.1.0 -f Dockerfile-python3.5-mono4.6.2-pythonnet2.1.0 .
+docker build -t cowboygneox/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python2.7-mono4.8.0-pythonnet2.2.2 .
+docker build -t cowboygneox/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python3.5-mono4.8.0-pythonnet2.2.2 .
 
-docker push cowboygneox/pythonnet:python2.7-mono4.6.2-pythonnet2.1.0
-docker push cowboygneox/pythonnet:python3.5-mono4.6.2-pythonnet2.1.0
+docker push cowboygneox/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2
+docker push cowboygneox/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2
